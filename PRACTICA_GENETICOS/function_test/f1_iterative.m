43147b9c75350a5806d200d35ef9ee11c74e41d0
JuanJoCZ <jose.cordoba@edu.uah.es>
2019-12-02 18:30:46 +0100

    ZIP Creado

--- PRACTICA_GENETICOS/function_test/f1_iterative.m
+++
@@ -1,7 +0,0 @@
-function y = f1_iterative (x)
-    acc = 0;
-    for i = 1:30
-       acc = acc + pow2(x(i));
-    end
-    y = acc;
-end
--- PRACTICA_GENETICOS/function_test/f3.m
+++
@@ -1,3 +0,0 @@
-function y = f3 (x)
-    y = sum(100*pow2(x(2:end)-pow2(x(1:end-1)))+pow2(x(1:end-1)-1));
-end
